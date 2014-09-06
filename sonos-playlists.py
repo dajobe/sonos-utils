77fc6efe30659084fd90f90ebfe422da980e2df9
Dave Beckett <dave@dajobe.org>
2014-09-06 13:20:56 -0700

    add a class

--- sonos-playlists.py
+++ sonos-playlists.py
@@ -17,60 +17,81 @@ import requests.packages.urllib3.exceptions
 logger = logging.getLogger('sonos-playlists')
 
 
-def find_coordinator():
-  """ Find coordinator speaker """
-  for speaker in soco.discover():
-    if speaker.is_coordinator:
-      return speaker
-  return None
+class InspectSonosPlaylist(object):
+  def __init__(self):
+    self.speakers = None
+    self.coord = None
+    self.playlists = None
+
+  def find_coordinator(self):
+    """ Find coordinator speaker """
+    if self.coord is not None:
+      return self.coord
+
+    if self.speakers is None:
+      self.speakers = soco.discover()
+
+    for speaker in self.speakers:
+      if speaker.is_coordinator:
+        self.coord = speaker
+        return self.coord
+    return None
 
 
-def find_playlist(speaker, title):
-  """ Find playlist by title """
-  result = None
-  for playlist in speaker.get_sonos_playlists():
-    if playlist.title == title:
-      result = playlist
-      break
-
-  return result
+  def get_playlists(self, speaker):
+    """ Find sonos playlists """
+    if self.playlists is None:
+      self.playlists = speaker.get_sonos_playlists()
+    return self.playlists
 
 
-def inspect_playlist(speaker, playlist):
-  print "Found sonos playlist %s - '%s' " % (playlist.item_id, playlist.title)
+  def find_playlist(self, speaker, title):
+    """ Find sonos playlist by title """
+    result = None
+    for playlist in self.get_playlists(speaker):
+      if playlist.title == title:
+        result = playlist
+        break
 
-  batch_size=300
-  start=0
-  total=None
+    return result
 
-  playlist_items = []
-  while True:
-    out = speaker.browse(playlist, max_items = batch_size, start=start)
-    if total is None:
-      total = out.total_matches
-      print "Playlist %s has %d items" % (playlist.title, total)
 
-    playlist_items += out[:]
-    start += len(out)
-    if start >= total:
-      break
+  def inspect_playlist(self, speaker, playlist):
+    """ Inspect a sonos playlist """
+    print "Found sonos playlist %s - '%s' " % (playlist.item_id, playlist.title)
 
-  i = 1
-  duplicates = []
-  seen = dict()
-  for item in playlist_items:
-    print "%4d %s - %s / %s" % (i, item.title, item.album, item.creator)
-    if item.uri in seen:
-      print "  DUPLICATE: seen first at %d" % (seen[item.uri], )
-      duplicates.append((i, item, seen[item.uri]))
-    else:
-      seen[item.uri] = i
-    i += 1
+    batch_size = 300
+    start = 0
+    total = None
 
-  if len(duplicates) > 0:
-    print "\nPlaylist has %d duplicates" % (len(duplicates), )
-    for (i, item, first_i) in duplicates:
-      print "  %4d %s - %s / %s seen first at %d" % (i, item.title, item.album, item.creator, first_i)
+    playlist_items = []
+    while True:
+      out = speaker.browse(playlist, max_items = batch_size, start=start)
+      if total is None:
+        total = out.total_matches
+        print "Playlist %s has %d items" % (playlist.title, total)
+
+      playlist_items += out[:]
+      start += len(out)
+      if start >= total:
+        break
+
+    i = 1
+    duplicates = []
+    seen = dict()
+    for item in playlist_items:
+      print "%4d %s - %s / %s" % (i, item.title, item.album, item.creator)
+      if item.uri in seen:
+        print "  DUPLICATE: seen first at %d" % (seen[item.uri], )
+        duplicates.append((i, item, seen[item.uri]))
+      else:
+        seen[item.uri] = i
+      i += 1
+
+    if len(duplicates) > 0:
+      print "\nPlaylist has %d duplicates" % (len(duplicates), )
+      for (i, item, first_i) in duplicates:
+        print "  %4d %s - %s / %s seen first at %d" % (i, item.title, item.album, item.creator, first_i)
 
 
 def main():
@@ -97,15 +118,16 @@ def main():
     logging.basicConfig(format='%(levelname)s:%(name)s:%(message)s', level=level)
 
     try:
-      coord = find_coordinator()
+      isp = InspectSonosPlaylist()
+      coord = isp.find_coordinator()
       if coord is not None:
         for title in titles:
-          playlist = find_playlist(coord, title)
+          playlist = isp.find_playlist(coord, title)
           if playlist is None:
             logger.error("Could not find sonos playlist with title '%s'", title)
             sys.exit(1)
           else:
-            inspect_playlist(coord, playlist)
+            isp.inspect_playlist(coord, playlist)
     except requests.packages.urllib3.exceptions.ProtocolError, e:
       logger.error("Network error: %s",str(e))
       sys.exit(1)
